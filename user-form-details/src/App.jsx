b2202831598b3e80a7ba85343cd84aa60d12d23f
AjiThHubgit <ajithfl02@gmail.com>
2025-02-26 22:41:58 +0530

    initial setup

---
+++ user-form-details/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Button } from 'react-bootstrap';
+
+function App() {
+
+  return (
+    <>
+      <div className="container mt-5">
+        <h1>Hello, React Bootstrap!</h1>
+        <Button variant="primary">Click Me</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
